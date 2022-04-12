6a0d70cfcef289e75473c944c0ae21df86e67e60
Ajil <ajiljagadeesh7@gmail.com>
2022-04-12 06:14:19 +0530

    feat(Master): added MUI | made changes to UI

--- src/App.js
+++ src/App.js
@@ -2,101 +2,124 @@ import { gapi } from "gapi-script";
 import { useEffect, useState } from "react";
 import { Login } from "./components/Login";
 import useDrivePicker from "react-google-drive-picker";
+import { Button } from "@mui/material";
 
-const SCOPES = 'https://www.googleapis.com/auth/drive' 
-const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'];
+const SCOPES = "https://www.googleapis.com/auth/drive";
+const DISCOVERY_DOCS = [
+  "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
+];
 function App() {
   const [openPicker, data, authResponse] = useDrivePicker();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isFetchingGoogleDriveFiles,setIsFetchingGoogleDriveFiles]=useState(false)
-  const[documents,setDocuments] = useState([])
-  const [listDocumentsVisibility,setListDocumentsVisibility] = useState(false)
+  const [isFetchingGoogleDriveFiles, setIsFetchingGoogleDriveFiles] =
+    useState(false);
+  const [documents, setDocuments] = useState([]);
+  const [listDocumentsVisibility, setListDocumentsVisibility] = useState(false);
   useEffect(() => {
     const start = () => {
       gapi.client.init({
         apiKey: process.env.REACT_APP_API_KEY,
         clientId: process.env.REACT_APP_CLIENT_ID,
         scope: SCOPES,
-        discoveryDocs:DISCOVERY_DOCS
-      })
-    }
-    gapi.load('client:auth2',start)
-  },[])
+        discoveryDocs: DISCOVERY_DOCS,
+      });
+    };
+    gapi.load("client:auth2", start);
+  }, []);
   const createFile = (tag) => {
     let accessToken = gapi.auth.getToken().access_token;
 
-    fetch('https://docs.googleapis.com/v1/documents',{
-      method: 'POST',
-      headers: new Headers({'Authorization': `Bearer ${accessToken}`}),
-    }).then((res) => res.json).then((data)=>{
-      console.log(data)
-      console.log(data.documentId)
+    fetch("https://docs.googleapis.com/v1/documents", {
+      method: "POST",
+      headers: new Headers({ Authorization: `Bearer ${accessToken}` }),
     })
-  }
+      .then((res) => res.json)
+      .then((data) => {
+        console.log(data);
+        console.log(data.documentId);
+      });
+  };
   const handleOpenPicker = () => {
     openPicker({
       clientId: process.env.REACT_APP_CLIENT_ID,
       developerKey: process.env.REACT_APP_API_KEY,
-      viewId: 'DOCS',
-      token:gapi.auth.getToken().access_token,
+      viewId: "DOCS",
+      token: gapi.auth.getToken().access_token,
       showUploadView: true,
       showUploadFolders: true,
       supportDrives: true,
       multiselect: true,
-    })
-  }
+    });
+  };
 
   const listFiles = (searchTerm = null) => {
     gapi.client.drive.files
-      .list({ fields: 'nextPageToken, files(id, name, parents, mimeType, modifiedTime,thumbnailLink,iconLink,hasThumbnail,webContentLink,webViewLink)',q:"'root' in parents" })
+      .list({
+        fields:
+          "nextPageToken, files(id, name, parents, mimeType, modifiedTime,thumbnailLink,iconLink,hasThumbnail,webContentLink,webViewLink)",
+        q: "'root' in parents",
+      })
       .then(function (response) {
         const res = JSON.parse(response.body);
         setDocuments(res?.files);
-        console.log(res)
+        console.log(res);
       });
   };
-  console.log(documents)
+  console.log(documents);
   useEffect(() => {
     if (data) {
-      data.docs.map(i=>console.log(i))
+      data.docs.map((i) => console.log(i));
     }
-  },[data])
+  }, [data]);
 
   return (
     <div className="App">
-     <Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} setDocuments={setDocuments} listFiles={listFiles}/>
-     {
-       isLoggedIn?
-       <div className="">
-     <button onClick={() => createFile('New File') }>Create File</button>
-     <button onClick={handleOpenPicker}>Open Picker</button>
-    
-       </div>:''
-     }
-     {
-        isFetchingGoogleDriveFiles? <>fetching</>:
-        <div style={{
-          display:'grid',
-          gridTemplateColumns:'repeat(auto-fit,minmax(280px,1fr))',
-          gridGap:'10px'
-        }}>
-
-          {documents.map(i=>
-          <div style={{
-            display:'flex',
-            backgroundColor:'#eee',
-            borderRadius:'5px',
-            padding:'10px',
-            gap:'10px',
-            cursor:'pointer'
-          }}
-            onClick={()=>i.webViewLink?window.open(i.webViewLink):''}
-          >
-            <img src={i.iconLink} width={20} height={25}/>
-            {i.name}
-          </div>)}
+      <Login
+        isLoggedIn={isLoggedIn}
+        setIsLoggedIn={setIsLoggedIn}
+        setDocuments={setDocuments}
+        listFiles={listFiles}
+      />
+      {isLoggedIn ? (
+        <div className="flex gap-5 mb-5">
+          <Button variant="contained" onClick={() => createFile("New File")}>
+            Create File
+          </Button>
+          <Button variant="contained" onClick={handleOpenPicker}>
+            Open Picker
+          </Button>
         </div>
-     }
+      ) : (
+        ""
+      )}
+      {isFetchingGoogleDriveFiles ? (
+        <>fetching</>
+      ) : (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fit,minmax(280px,1fr))",
+            gridGap: "10px",
+          }}
+        >
+          {documents.map((i) => (
+            <div
+              style={{
+                display: "flex",
+                backgroundColor: "#eee",
+                borderRadius: "5px",
+                padding: "10px",
+                gap: "10px",
+                cursor: "pointer",
+              }}
+              onClick={() => (i.webViewLink ? window.open(i.webViewLink) : "")}
+            >
+              <img src={i.iconLink} width={20} height={25} />
+              {i.name}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,41 +1,49 @@
-import React, { useState } from 'react'
-import { GoogleLogin,GoogleLogout } from 'react-google-login'
+import React, { useState } from "react";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
 
-const clientId = process.env.REACT_APP_CLIENT_ID
-export const Login = ({isLoggedIn,setIsLoggedIn,setDocuments,listFiles}) => {
-    
-    const onSuccess = (response) => {
-        setIsLoggedIn(true)
-        listFiles()
-        console.log('Login Successful', response.profileObj)
-    }
-    const onFailure = (response) => {
-        console.log('Login Failed', response)
-    }
-    
-    return (
-        <div className={!isLoggedIn?'flex justify-center items-center h-screen':'flex justify-end'}>
-            {
-                isLoggedIn ? 
-                <GoogleLogout
-                    clientId={clientId}
-                    buttonText="Logout"
-                    onLogoutSuccess={() => {
-                        setIsLoggedIn(false)
-                        setDocuments([])
-                    }
-                    }
-                />
-                :<GoogleLogin 
-                    clientId={clientId} 
-                    buttonText="Login" 
-                    onSuccess={onSuccess} 
-                    onFailure={onFailure} 
-                    cookiePolicy='single_host_origin' 
-                    isSignedIn={true}
-                />
-            }
-            
-        </div>
-    )
-}
+const clientId = process.env.REACT_APP_CLIENT_ID;
+export const Login = ({
+  isLoggedIn,
+  setIsLoggedIn,
+  setDocuments,
+  listFiles,
+}) => {
+  const onSuccess = (response) => {
+    setIsLoggedIn(true);
+    listFiles();
+    console.log("Login Successful", response.profileObj);
+  };
+  const onFailure = (response) => {
+    console.log("Login Failed", response);
+  };
+
+  return (
+    <div
+      className={
+        !isLoggedIn
+          ? "flex justify-center items-center h-screen"
+          : "flex justify-end"
+      }
+    >
+      {isLoggedIn ? (
+        <GoogleLogout
+          clientId={clientId}
+          buttonText="Logout"
+          onLogoutSuccess={() => {
+            setIsLoggedIn(false);
+            setDocuments([]);
+          }}
+        />
+      ) : (
+        <GoogleLogin
+          clientId={clientId}
+          buttonText="Login"
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          cookiePolicy="single_host_origin"
+          isSignedIn={true}
+        />
+      )}
+    </div>
+  );
+};
